485848e234e1d07c1c1336f9bbec647cac591785
Kieran O'Reilly <michael.kino.oreilly@gmail.com>
2024-01-19 07:51:34 +0100

    Add unnamedplus to neovim clipboard

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set clipboard to system clipboards :)
+vim.opt.clipboard = 'unnamedplus'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
